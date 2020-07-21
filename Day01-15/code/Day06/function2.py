62314826b55979e3f6fa8d624c80cb965c04ade2
e <2277920562@qq.com>
2020-07-21 10:04:57 +0800

    add comments

--- Day01-15/code/Day06/function2.py
+++ Day01-15/code/Day06/function2.py
@@ -6,7 +6,7 @@ Author: 骆昊
 Date: 2018-03-05
 """
 
-
+""" 
 def gcd(x, y):
     if x > y:
         (x, y) = (y, x)
@@ -22,3 +22,18 @@ def lcm(x, y):
 
 print(gcd(15, 27))
 print(lcm(15, 27))
+ """
+ 
+def gcd(x, y):
+    """求最大公约数"""
+    (x, y) = (y, x) if x > y else (x, y)      #if-else的简写
+    for factor in range(x, 0, -1):
+        if x % factor == 0 and y % factor == 0:
+            return factor
+
+
+def lcm(x, y):
+    """求最小公倍数"""
+    return x * y // gcd(x, y)
+
+print(gcd(15,25))
